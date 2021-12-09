94ea3005273d4c5f656cc2f2e62080a0ebc02639
Roberto amato neto <66858040+robertoamato@users.noreply.github.com>
2021-12-09 19:22:55 -0300

    exercicios do curso do guanabara

---
+++ exercicios.js/AULA 11 PARTE 1/exec001.js
@@ -0,0 +1,6 @@
+var vel = 70
+    console.log(`a velocidade do seu carro é ${vel} KM/H`)
+if (vel>60){
+    console.log(`VOce ultrapassou o limite de velocidade. Multado`)
+}
+    console.log(`Voce esta andando na velocidade permitida siga em frente`)
---
+++ exercicios.js/AULA 11 PARTE 1/exec002.js
@@ -0,0 +1,12 @@
+var pais ='brasil'
+console.log(`vivendo em ${pais}`)
+
+if (pais == 'brasil')
+{
+    console.log(`voce é brasileiro`)
+}
+else{
+console.log('estrageiro')
+
+
+}
