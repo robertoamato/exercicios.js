e47a7cc8ce11d58c5d77f25e5187d540b794e95b
robertoamato <robertoamatoz@gmail.com>
2024-10-08 18:22:40 -0300

    Create tipagemfraca.js

---
+++ exercicios.js/TIPAGEM FRACA JS/tipagemfraca.js
@@ -0,0 +1,7 @@
+let teste = 'qualquer';  // Declara uma variável chamada 'teste' e atribui a ela o valor 'qualquer', que é um texto (string).
+console.log(teste);  // Exibe o valor da variável 'teste' no console, que será 'qualquer'.
+console.log(typeof teste);  // Exibe o tipo do valor armazenado na variável 'teste'. Nesse caso, será 'string', já que 'qualquer' é um texto.
+
+teste = 1234;  // A variável 'teste' agora recebe um novo valor: 1234, que é um número.
+console.log(teste);  // Exibe o novo valor da variável 'teste' no console, que será 1234.
+console.log(typeof teste);  // Exibe o tipo do valor armazenado na variável 'teste'. Agora, será 'number', porque 1234 é um número.
