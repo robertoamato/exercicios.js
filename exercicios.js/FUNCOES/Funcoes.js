95595a3e3009a7fe7ccb76d464f798521ee9a0f9
robertoamato <robertoamatoz@gmail.com>
2024-10-10 17:32:39 -0300

    Update Funcoes.js

--- exercicios.js/FUNCOES/Funcoes.js
+++ exercicios.js/FUNCOES/Funcoes.js
@@ -1,16 +1,14 @@
 function SaudeFinanceira(ganhos, gastos) {
-  if (ganhos > gastos) {
+  if (ganhos > gastos ) {
     return 'Voce esta no azul'
-    
-  }else{
+  } else {
     return 'Voce esta no vermelho'
-
   }
- 
 }
 
-const maria = SaudeFinanceira (1000,11000);  
-const pedro = SaudeFinanceira (2999, 1000)
-
+const maria = SaudeFinanceira(1000, 11000)
+const pedro = SaudeFinanceira(2999, 1000)
+const roberto = SaudeFinanceira (10000, 3500)
 console.log(maria)
 console.log(pedro)
+console.log(roberto)
